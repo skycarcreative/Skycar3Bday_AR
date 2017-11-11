e2871ab3d2ead06f07c29582f0dede393a12d931
Michael Burkard <burkarddev@gmail.com>
2017-11-11 08:54:39 +0800

    Upload first project

---
+++ Unity/Assets/FireworksDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworksDestroyer : MonoBehaviour {
+
+	[SerializeField] float m_lifeTime;
+
+	public float m_curTime;
+
+
+	void Update () {
+
+		if (m_curTime < m_lifeTime) {
+
+			m_curTime += Time.deltaTime;
+		} else {
+
+			m_curTime = 0f;
+			gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Unity/Assets/TriggerFierworks.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFierworks : MonoBehaviour {
+
+	[SerializeField] Animator m_skyCar;
+	[SerializeField] Transform m_car;
+	[SerializeField] GameObject[] m_fireworks;
+	public float m_curTime;
+
+	void Start () {
+
+		m_skyCar = m_car.GetComponent<Animator> ();
+	}
+
+
+	void Update () {
+
+		foreach (GameObject o in m_fireworks) {
+			
+			if (!o.activeSelf)
+				
+				o.SetActive (m_skyCar.GetCurrentAnimatorStateInfo (0).normalizedTime % 1f > .5f && m_skyCar.GetCurrentAnimatorStateInfo (0).normalizedTime % 1f < .6f);
+		}
+	}
+}
