ad1cc5c790cc421a1aa1c4e3aea0f0c0adc70748
JH Lee <jhpassion0621@hotmail.com>
2018-05-27 03:03:41 +0800

    Update ARKit plugin

--- Unity/Assets/UnityARKitPlugin/ARKitRemote/ARKitRemoteConnection.cs
+++ Unity/Assets/UnityARKitPlugin/ARKitRemote/ARKitRemoteConnection.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking.PlayerConnection;
 using System.Text;
-using Utils; 
+using UnityEngine.XR.iOS.Utils; 
 
 #if UNITY_EDITOR
  
@@ -16,6 +16,7 @@ namespace UnityEngine.XR.iOS
 		public UnityARPlaneDetection planeDetection = UnityARPlaneDetection.Horizontal;
 		public bool getPointCloud = true;
 		public bool enableLightEstimation = true;
+		public bool enableAutoFocus = true;
 
 		[Header("Run Options")]
 		public bool resetTracking = true;
@@ -88,8 +89,10 @@ namespace UnityEngine.XR.iOS
 
 		void OnDestroy()
 		{
-			#if UNITY_2017_1_OR_NEWER		
-			editorConnection.DisconnectAll ();
+			#if UNITY_2017_1_OR_NEWER
+			if(editorConnection != null) {
+				editorConnection.DisconnectAll ();
+			}
 			#endif
 		}
 
@@ -184,7 +187,7 @@ namespace UnityEngine.XR.iOS
 		{
 			serializableFromEditorMessage sfem = new serializableFromEditorMessage ();
 			sfem.subMessageId = SubMessageIds.editorInitARKit;
-			serializableARSessionConfiguration ssc = new serializableARSessionConfiguration (startAlignment, planeDetection, getPointCloud, enableLightEstimation); 
+			serializableARSessionConfiguration ssc = new serializableARSessionConfiguration (startAlignment, planeDetection, getPointCloud, enableLightEstimation, enableAutoFocus); 
 			UnityARSessionRunOption roTracking = resetTracking ? UnityARSessionRunOption.ARSessionRunOptionResetTracking : 0;
 			UnityARSessionRunOption roAnchors = removeExistingAnchors ? UnityARSessionRunOption.ARSessionRunOptionRemoveExistingAnchors : 0;
 			sfem.arkitConfigMsg = new serializableARKitInit (ssc, roTracking | roAnchors);
